3769b78637022bc52dce33a47559b46d4a8f4a8a
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-01-17 16:41:44 -0300

    use separated instance storage variables

--- instance-vector-heavy/src/test.rs
+++ instance-vector-heavy/src/test.rs
@@ -11,9 +11,12 @@ fn test() {
     let contract_id = env.register_contract(None, InstanceVectorHeavyContract);
     let client = InstanceVectorHeavyContractClient::new(&env, &contract_id);
 
-    client.increment();
-    assert_eq!(client.get_address(&0), client.address.clone());
+    client.increment_a();
+    assert_eq!(client.get_address_a(&0), client.address.clone());
 
-    client.increment();
-    assert_eq!(client.get_address(&1), client.address.clone());
+    client.increment_a();
+    assert_eq!(client.get_address_a(&1), client.address.clone());
+
+    client.increment_b();
+    assert_eq!(client.get_address_b(&0), client.address.clone());
 }
