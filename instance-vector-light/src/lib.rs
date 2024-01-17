31ec1a05d20b1e8c3ea5c05e6460d6a1acb19b34
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-01-17 16:43:37 -0300

    use separated instance storage variables

--- instance-vector-light/src/lib.rs
+++ instance-vector-light/src/lib.rs
@@ -1,29 +1,50 @@
 #![no_std]
 use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol, Vec, Address};
 
-const VECTOR: Symbol = symbol_short!("VECTOR");
-
+const VECTOR_A: Symbol = symbol_short!("VECTOR");
+const VECTOR_B: Symbol = symbol_short!("VECTOR");
+ 
 #[contract]
 pub struct InstanceVectorLightContract;
 
 #[contractimpl]
 impl InstanceVectorLightContract {
     /// Increment increments the vector, adding more informarion
-    pub fn increment(env: Env) {
+    pub fn increment_a(env: Env) {
         // Get the current vecot.
         let mut vector: Vec<Address> = env.storage().instance()
-            .get(&VECTOR).unwrap_or(Vec::new(&env)); // If no value set, assume an empty vector.
+            .get(&VECTOR_A).unwrap_or(Vec::new(&env)); // If no value set, assume an empty vector.
 
         // Push the current contract address in the vector
         vector.push_back(env.current_contract_address().clone());
 
         // Save the updated vector to instance storage
-        env.storage().instance().set(&VECTOR, &vector);
+        env.storage().instance().set(&VECTOR_A, &vector);
     }
 
-    pub fn get_address(env: Env, n: u32) -> Address{
+    pub fn increment_b(env: Env) {
+        // Get the current vecot.
+        let mut vector: Vec<Address> = env.storage().instance()
+            .get(&VECTOR_B).unwrap_or(Vec::new(&env)); // If no value set, assume an empty vector.
+
+        // Push the current contract address in the vector
+        vector.push_back(env.current_contract_address().clone());
+
+        // Save the updated vector to instance storage
+        env.storage().instance().set(&VECTOR_B, &vector);
+    }
+
+    pub fn get_address_a(env: Env, n: u32) -> Address{
         let vector = env.storage().instance()
-        .get(&VECTOR).unwrap_or(Vec::new(&env));
+        .get(&VECTOR_A).unwrap_or(Vec::new(&env));
+
+        vector.get(n).unwrap()
+
+    }
+
+    pub fn get_address_b(env: Env, n: u32) -> Address{
+        let vector = env.storage().instance()
+        .get(&VECTOR_B).unwrap_or(Vec::new(&env));
 
         vector.get(n).unwrap()
 
